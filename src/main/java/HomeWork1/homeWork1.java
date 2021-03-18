626fc86ce67e325b9814ea8c6a596fd6ccdc7cc5
siba74 <siba740@mail.ru>
2021-03-18 23:09:07 +0500

    Initial commit

---
+++ src/main/java/HomeWork1/homeWork1.java
@@ -0,0 +1,13 @@
+package HomeWork1;
+
+public class homeWork1 {
+
+    static double task1(double a, double b, double c, double d) {
+        return a * (b + (c / d));
+    }
+
+    public static void  main(String[] args) {
+        System.out.println(task1(2, 2,0,0));
+
+    }
+}
