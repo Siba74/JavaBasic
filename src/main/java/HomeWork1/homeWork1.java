221c333cc78940f49773c44cf1b42d72c45d3822
siba74 <siba740@mail.ru>
2021-03-22 22:26:26 +0500

    HomeWork1 Не разобрался с Заданием №8

--- src/main/java/HomeWork1/homeWork1.java
+++ src/main/java/HomeWork1/homeWork1.java
@@ -1,13 +1,50 @@
 package HomeWork1;
 
+import java.util.GregorianCalendar;
+
 public class homeWork1 {
 
-    static double task1(double a, double b, double c, double d) {
+    //Задание №3
+    static float task3(float a, float b, float c, float d) {
         return a * (b + (c / d));
     }
 
-    public static void  main(String[] args) {
-        System.out.println(task1(2, 2,0,0));
+    //Задание №4
+    static boolean task4(int a, int b) {
+        int sum = a + b;
+        return sum >= 10 && sum <= 20;
+    }
 
+    static void task5(int value) {
+        // если параметр велью больше нуля вывести Положительное
+        // иначе если параметр равен нулю вывести Ноль
+        // иначе вывести Отрицательное
+        if (value > 0) {
+            System.out.println("Положительное");
+        } else if (value == 0) {
+            System.out.println("Ноль");
+        } else {
+            System.out.println("Отрицательное");
+        }
+    }
+
+    //Задание №6
+    static boolean task6(int x) {
+        return x <= 0;
+    }
+
+    //Задание №7
+    static void task7(String name) {
+        System.out.println("Привет, " + name);
+    }
+
+    public static void  main(String[] args) {
+        System.out.println("Задание №3 = " + task3(5f, 10f,12f,15.6f));
+        System.out.println("Задание №4 = " + task4(10, 7));
+        System.out.print("Задание №5  = ");
+        task5(5);
+        System.out.println("Задание №6 = " + task6(-6));
+        System.out.print("Задание №7  = ");
+        task7("Ramil");
     }
 }
